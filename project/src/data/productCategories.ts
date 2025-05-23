059108470c4267b817ff808028ebe4c0f1094071
Purushotham-Mutyala <uthammutyala@gmail.com>
2025-05-23 15:25:57 +0530

    done with the code part

--- project/src/data/productCategories.ts
+++
@@ -1,9 +0,0 @@
-export const productCategories = [
-  { id: 'living-room', name: 'Living Room' },
-  { id: 'bedroom', name: 'Bedroom' },
-  { id: 'dining', name: 'Dining' },
-  { id: 'office', name: 'Home Office' },
-  { id: 'outdoor', name: 'Outdoor' },
-  { id: 'storage', name: 'Storage' },
-  { id: 'decor', name: 'Decor' },
-];
